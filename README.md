11db5314d5119f7b793de30f1cbdc0d6a46c428c
Guillaume Legare <g23leg@gmail.com>
2023-09-08 12:55:24 -0400

    add components: header, footer & card + css update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TP3inte
  SG produits ecologiques
+
+
+Tailwind Components:
+
+1-Header
+2-Footer
+3-Card dans animaux.html
--- script.js
+++
@@ -1,9 +0,0 @@
-const primaryNav = document.getElementById('primary-navigation');
-const navToggle = document.getElementById('mobile-nav-toggle');
-
-navToggle.addEventListener('click', () => {
-  const visibility = primaryNav.getAttribute('data-visible');
-
-  console.log(visibility);
-});
-
