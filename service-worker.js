a79c6afde7aeba87c14a15626002652bb0aad1a1
Guillaume Legare <g23leg@gmail.com>
2023-09-12 12:28:46 -0400

    service worker offline handler

--- service-worker.js
+++ service-worker.js
@@ -18,11 +18,34 @@ self.addEventListener('install', (evt) => {
 
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
+
   //Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key)
+        }
+      }));
+    })
+  );
+
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
+
   //Add fetch event handler here.
+  if (evt.request.mode !== 'navigate') {
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match('/TP3inte/offline.html')
+      })
+    })
+  )
 });
